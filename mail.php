aea93187536bcf6501a80f325fa518de4ef1faca
mattmannec <mattmannec@gmail.com>
2020-07-13 15:55:08 +0200

    send email functionnally works

--- mail.php
+++ mail.php
@@ -1,18 +1,22 @@
 <?php
-$email = "dortwag@gmail.com";
+
+$email = $_POST['email'];
+$firstname = $_POST['firstname'];
+
+$destination = "dortwag@gmail.com";
 $headers = ['From' => $email, 
             'Reply-To' => $email, 
             'Content-type' => 'text/html; charset=iso-8859-1',
         ];
 
 // the message
-$msg = "First line of text\nSecond line of text";
+$msg = $firstname . "would like to know more about ADT Security, please get in contact with them. " . $email;
 
 // use wordwrap() if lines are longer than 70 characters
 $msg = wordwrap($msg,70);
 
 // send email
-if(mail($email, "My subject", $msg, $headers)){
+if(mail($destination, "My subject", $msg, $headers)){
     echo('true');
 } else {
     echo('false');
